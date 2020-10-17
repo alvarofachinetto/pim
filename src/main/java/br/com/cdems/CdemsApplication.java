f61bf8a92764e01f2295a69774b710e794ab373f
Alvaro Silva <alvaro.facneto@gmail.com>
2020-10-17 20:06:24 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/cdems/CdemsApplication.java
@@ -0,0 +1,13 @@
+package br.com.cdems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cdems/CdemsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cdems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
